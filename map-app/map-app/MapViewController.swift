76060ad082e742d06130ff5407afa2d39d977224
Elle Gover <elle.gover@detroitlabs.com>
2018-10-02 16:20:23 -0400

    added Marks to MapViewController

--- map-app/map-app/MapViewController.swift
+++ map-app/map-app/MapViewController.swift
@@ -11,18 +11,22 @@ import Mapbox
 
 class MapViewController: UIViewController {
 
+    // MARK: - Override Methods
+   
     override func viewDidLoad() {
         super.viewDidLoad()
 
         populateMap()
     }
  
+    // MARK: - Methods
+    
     func populateMap() {
         
         let url = URL(string: "mapbox://styles/mapbox/streets-v10")
         let mapView = MGLMapView(frame: view.bounds, styleURL: url)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        mapView.setCenter(CLLocationCoordinate2D(latitude: 42.371373, longitude: -83.066100), zoomLevel: 10.93, animated: false)
+        mapView.setCenter(CLLocationCoordinate2D(latitude: 42.371373, longitude: -83.066100), zoomLevel: 9, animated: false)
         view.addSubview(mapView)
     }
     
