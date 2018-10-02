5de971162755d1c4ce4dc8aebbda834cd176ca89
Britney Smith <brit.smith7@gmail.com>
2018-10-02 09:54:52 -0400

    initial commit: Mapbox pod and access token set up

---
+++ map-app/map-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  map-app
+//
+//  Created by Britney Smith on 10/2/18.
+//  Copyright © 2018 Britney Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
