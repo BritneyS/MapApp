41c8a6ef069ba5f9beea6a36817118c90e0f91b1
Kendall Poindexter <kendall.poindexter@detroitlabs.com>
2018-10-02 14:53:03 -0400

    Setup Data Source Protocols

--- map-app/map-app/HomeViewController.swift
+++ map-app/map-app/HomeViewController.swift
@@ -23,3 +23,19 @@ class HomeViewController: UIViewController {
 
 }
 
+//MARK: - TableView Data Source Methods
+
+extension HomeViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath)
+        return cell 
+    }
+    
+    
+}
+
