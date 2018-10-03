1adcf2cf020f202774eae175d2ec14331adeb6bc
Britney Smith <brit.smith7@gmail.com>
2018-10-03 15:09:12 -0400

    Added model to receive json data

---
+++ map-app/map-app/ParsedPark.swift
@@ -0,0 +1,35 @@
+//
+//  ParsedPark.swift
+//  map-app
+//
+//  Created by Britney Smith on 10/3/18.
+//  Copyright © 2018 Britney Smith. All rights reserved.
+//
+
+import Foundation
+
+class ParsedPark: Codable {
+    var name: String?
+    var the_geom: Geometry
+    
+}
+
+class Geometry: Codable {
+    var coordinates: [[[[Double?]]]] = [
+                [
+                    [
+                        [1.00, 2.00]
+                    ]
+                ]
+    ]
+    
+    var lat: Double {
+        guard let latitude = coordinates[0][0][0][1] else { return 0.0 }
+        return latitude
+    }
+    
+    var lon: Double {
+        guard let longitude = coordinates[0][0][0][0] else { return 0.0 }
+        return longitude
+    }
+}
